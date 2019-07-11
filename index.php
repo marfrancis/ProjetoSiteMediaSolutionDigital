fc61978d09ff3714bfb732ff1fbe66d89dd5d574
marfrancis <marcelo@mediasolutionproducoes.com.br>
2019-07-11 17:55:34 -0300

    retirado testemonial da index

--- index.php
+++ index.php
@@ -221,68 +221,7 @@ include_once 'head.php'?>
         </div>
         <!-- About Us Area End -->
 
-        <!-- Testimonial Area Start -->
-        <div class="testimonial-area section-pb">
-            <div class="container">
-                <div class="row ">
-                    <div class="col-lg-12 mr-auto ml-auto testimonial-active">
-                        <!-- testimonial-wrap Start -->
-                        <div class="testimonial-wrap text-center">
-                            <div class="testimonial-image ">
-                                <img src="assets/images/review/comment-3.jpg" alt="">
-                            </div>
-                            <div class="testimonial-content">
-                                <ul>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start-o.png" alt=""></a></li>
-                                </ul>
-                                <p>It is a long established fact that a reader will be distracted by the readable
-                                    content of a page when looking at its layout.
-                                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-                                    letters,page when looking at its layout. The point</p>
-
-                            </div>
-                            <div class="author-info">
-                                <h4>Anna Rose</h4>
-                                <p>Fassion Designer</p>
-                            </div>
-                        </div>
-                        <!--// testimonial-wrap End -->
-
-                        <!-- testimonial-wrap Start -->
-                        <div class="testimonial-wrap text-center">
-                            <div class="testimonial-image ">
-                                <img src="assets/images/review/comment-2.jpg" alt="">
-                            </div>
-                            <div class="testimonial-content">
-                                <ul>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start.png" alt=""></a></li>
-                                    <li><a href="#"><img src="assets/images/icon/start-o.png" alt=""></a></li>
-                                </ul>
-                                <p>It is a long established fact that a reader will be distracted by the readable
-                                    content of a page when looking at its layout.
-                                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-                                    letters,page when looking at its layout. The point</p>
-
-                            </div>
-                            <div class="author-info">
-                                <h4>Rose Jorina</h4>
-                                <p>Fassion Designer</p>
-                            </div>
-                        </div>
-                        <!--// testimonial-wrap End -->
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Testimonial Area End -->
+        <?php include_once 'perfect.php' ?>
 
         <!-- Latest News Area Start -->
         <div class="latest-news-area section-pt section-pb-80  bg-grey">
